755972c703d5054bf6a952eb9b075282214c50a9
Lucas Vinicius Moura Costa <58613215+LucasVinicius-sudo@users.noreply.github.com>
2022-09-26 17:35:39 -0300

    Create watermelon.cpp

---
+++ watermelon.cpp
@@ -0,0 +1,21 @@
+include <iostream>
+using namespace std;
+int main()
+{
+ 
+    int num;
+    cin >> num;
+    if(num % 2 == 0 && num > 2)
+    {
+ 
+      cout << "YES" << endl;
+    }
+    else
+    {
+ 
+      cout << "NO" << endl;
+    }
+ 
+ 
+  return 0;
+}
